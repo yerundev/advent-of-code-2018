d4497c60dbcfc1ad61ec9ee1b1c79a65f8b3423d
Ludwig <ludwig.winblad@gmail.com>
2018-12-02 17:48:37 +0100

    Initial draft of day two

---
+++ src/Two/TwoCommand.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Aoc\Two;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+class TwoCommand extends Command
+{
+    private $data;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->data = [];
+    }
+
+    protected function configure()
+    {
+        $this->setName('day-two')
+            ->setDescription('Run day two of AoC 2018')
+            ->addArgument('input', InputArgument::REQUIRED, 'The input file');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->readFile($input->getArgument('input'));
+
+        $totalOccurances = array_fill(2, 2, 0);
+
+        foreach ($this->data as $word) {
+            $occurances = array_unique(array_values($this->letterOccurances($word, 2, 3)));
+            foreach ($occurances as $occurance) {
+                $totalOccurances[$occurance]++;
+            }
+        }
+
+        $checksum = array_product($totalOccurances);
+        $output->writeln("Part one: {$checksum}");
+        $found = false;
+
+        foreach ($this->data as $index => $word) {
+            
+            if ($index == count($this->data) || $found) {
+                break;
+            }
+
+            $copy = $this->data;
+            unset($copy[$index]);
+
+            foreach ($copy as $other) {
+                $commonSubStr = $this->common($word, $other);
+                if (strlen($word) - strlen($commonSubStr) == 1) {
+                    $output->writeln("Part two: {$commonSubStr}");
+                    $found = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    private function letterOccurances(string $string, int $min, int $max)
+    {
+        $occurances = [];
+        
+        $letters = array_unique(str_split($string));
+
+        foreach ($letters as $letter) {
+            $count = substr_count($string, $letter);
+            if ($count >= $min && $count <= $max) {
+                $occurances[$letter] = $count;
+            }
+        }
+
+        return $occurances;
+    }
+
+    private function common(string $str1, string $str2)
+    {   
+        $nbrBottom = strspn($str1 ^ $str2, "\0");
+        $nbrTop = strspn(strrev($str1) ^ strrev($str2), "\0");
+
+        $bottom = substr($str1, 0, $nbrBottom);
+        $top = substr($str1, strlen($str1) - $nbrTop);
+        return $bottom . $top;
+        return count(array_diff(str_split($str1), str_split($str2)));
+    }
+
+    private function readFile(string $file)
+    {
+        if ($file = fopen($file, 'r')) {
+            while (!feof($file)) {
+                $line = fgets($file);
+                if (!empty($line)) {
+                    $this->data[] = $line;
+                }
+            }
+            fclose($file);
+        }
+    }
+}
